0ff01bbdf10255d754ed2af109d1948caa9c54d9
hscom96 <hscom96@gmail.com>
2020-03-20 11:40:48 +0900

    initial commit

---
+++ src/main/java/com/yapp/erooja/EroojaApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.erooja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EroojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EroojaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yapp/erooja/EroojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.erooja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EroojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
