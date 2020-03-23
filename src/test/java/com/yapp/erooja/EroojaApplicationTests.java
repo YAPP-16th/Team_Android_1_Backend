a827ccbe9fecb2c1c646ab71cf38f7f78fe0e72e
김승신 <siotman0728@gmail.com>
2020-03-23 22:56:06 +0900

    초기 프로젝트 CI 통과를 위한 JPA 기본 셋팅과 프로파일 셋업

--- src/test/java/com/yapp/erooja/EroojaApplicationTests.java
+++ src/test/java/com/yapp/erooja/EroojaApplicationTests.java
@@ -2,8 +2,10 @@ package com.yapp.erooja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class EroojaApplicationTests {
 
     @Test
