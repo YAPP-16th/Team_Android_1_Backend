b381b55c2ab4ae2ab48e0591093d875b368c2e1e
김승신 <siotman0728@gmail.com>
2020-04-12 02:35:44 +0900

    이루자 어드민 토큰 엔드포인트

---
+++ src/main/java/com/eroom/erooja/features/auth/erooja/dto/AuthDTO.java
@@ -0,0 +1,11 @@
+package com.eroom.erooja.features.auth.erooja.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AuthDTO {
+    private String uid;
+    private String password;
+}
