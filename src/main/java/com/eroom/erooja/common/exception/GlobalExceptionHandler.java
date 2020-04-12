079e263ece8a4a186009b1c7d66365f63eeac7f8
김승신 <siotman0728@gmail.com>
2020-04-12 20:57:47 +0900

    로깅 관련 설정 및 예외 로깅 처리

--- src/main/java/com/eroom/erooja/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eroom/erooja/common/exception/GlobalExceptionHandler.java
@@ -23,11 +23,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public void handleEroojaException(HttpServletRequest request, HttpServletResponse response, EroojaException ex) throws IOException {
         ErrorEnum.ErrorResponse errorResponse = ex.getErrorEnum().getErrorResponse();
         response.sendError(ex.getStatus().value(), errorResponse.getMessage());
+        logger.error("Erooja 오류 감지. - Enum : {}, message : {}", ex.getMessage(), errorResponse.getMessage());
     }
 
     /* 외부 라이브러리 예외 처리 정의 */
     @ExceptionHandler({ JwtException.class })
     public void handleJwtException(HttpServletRequest request, HttpServletResponse response, JwtException ex) throws IOException {
+        logger.error("잘못된 JWT 감지. - message : {}, cause : {}", ex.getMessage(), ex.getCause());
+
         if ((ex instanceof ExpiredJwtException)) {
             handleEroojaException(request, response, new EroojaException(ErrorEnum.JWT_EXPIRED));
         } else if (ex instanceof MalformedJwtException) {
@@ -35,9 +38,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         } else if (ex instanceof UnsupportedJwtException) {
             handleEroojaException(request, response, new EroojaException(ErrorEnum.JWT_UNSUPPORTED));
         } else {
-            logger.error("JWT Error invoked - message : {}, cause : {}", ex.getMessage(), ex.getCause());
             handleEroojaException(request, response, new EroojaException(ErrorEnum.JWT_UNKNOWN_ERROR));
         }
     }
     /* 외부 라이브러리 예외 처리 정의 끝 */
+
+    @ExceptionHandler({ Exception.class })
+    public void handleException(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException {
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+        logger.error("알 수 없는 오류 감지. - message : {}, cause : {}", ex.getMessage(), ex.getCause());
+    }
 }
