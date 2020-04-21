e263738e374b801164a413f12b8f7ef6375338e2
hscom96 <hscom96@gmail.com>
2020-04-22 01:07:42 +0900

    할일 우선순위 검증 구현 & 테스트코드

--- src/main/java/com/eroom/erooja/features/todo/service/TodoService.java
+++ src/main/java/com/eroom/erooja/features/todo/service/TodoService.java
@@ -1,5 +1,6 @@
 package com.eroom.erooja.features.todo.service;
 
+import com.eroom.erooja.common.exception.EroojaException;
 import com.eroom.erooja.domain.model.MemberGoal;
 import com.eroom.erooja.domain.model.Todo;
 import com.eroom.erooja.features.todo.dto.TodoDTO;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.eroom.erooja.common.constants.ErrorEnum.TODO_PRIORITY_NOT_CORRECT;
+
 @RequiredArgsConstructor
 @Service
 public class TodoService {
@@ -18,15 +21,29 @@ public class TodoService {
     private final ModelMapper modelMapper;
 
     public List<Todo> addTodo(String uid, Long goalId, List<TodoDTO> todoDTOList) {
-        List<Todo> todoList = todoDTOList.stream()
-                .map(todoDTO -> {
-                    Todo todo = modelMapper.map(todoDTO, Todo.class);
-                    todo.setIsEnd(false);
-                    todo.setMemberGoal(MemberGoal.builder().uid(uid).goalId(goalId).build());
-                    return todo;
-                })
-                .collect(Collectors.toList());
+        List<Todo> todoList = mapDTOtoTodo(uid, goalId, todoDTOList);
+
+        if (checkPriorityIsNotCorrect(todoList))
+            throw new EroojaException(TODO_PRIORITY_NOT_CORRECT);
 
         return todoRepository.saveAll(todoList);
+    }
+
+    public Boolean checkPriorityIsNotCorrect(List<Todo> todoList) {
+        int checkCount = 0;
+        for (Todo todo : todoList) {
+            if (todo.getPriority() != checkCount++)
+                return true;
+        }
+        return false;
+    }
+
+    public List<Todo> mapDTOtoTodo(String uid, Long goalId, List<TodoDTO> todoDTOList) {
+        return todoDTOList.stream()
+                .map(todoDTO -> {
+                    Todo todo = modelMapper.map(todoDTO, Todo.class);
+                    todo.setMemberGoal(MemberGoal.builder().uid(uid).goalId(goalId).build());
+                    return todo;
+                }).collect(Collectors.toList());
     }
 }
