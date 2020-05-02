299ab92e7c9dc0bed1df7354316fbd2ca7ce066a
ian-siotman <siotman0728@gmail.com>
2020-05-02 18:08:33 +0900

    투두 체크 레이트 롬복 디폴트 설정

--- src/main/java/com/eroom/erooja/domain/model/MemberGoal.java
+++ src/main/java/com/eroom/erooja/domain/model/MemberGoal.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.Builder.Default;
 
 @EqualsAndHashCode(of = {"id"})
 @NoArgsConstructor
@@ -44,6 +45,7 @@ public class MemberGoal extends AuditProperties {
     private LocalDateTime endDt;
 
     @Min(0) @Max(1)
+    @Default
     private Double checkedTodoRate = .0;
 
     @JsonIgnore
@@ -62,16 +64,17 @@ public class MemberGoal extends AuditProperties {
 
     @Builder
     public MemberGoal(LocalDateTime createDt, LocalDateTime updateDt, String uid,
-                      Long goalId, GoalRole role, Boolean isEnd, int copyCount,
+                      Long goalId, GoalRole role, Boolean isEnd, int copyCount, Double checkedTodoRate,
                       LocalDateTime startDt, LocalDateTime endDt) {
         super(createDt, updateDt);
         this.uid = uid;
-        this.goalId=goalId;
-        this.role=role;
-        this.isEnd=isEnd;
-        this.copyCount=copyCount;
-        this.startDt=startDt;
-        this.endDt=endDt;
+        this.goalId = goalId;
+        this.role = role;
+        this.isEnd = isEnd;
+        this.copyCount = copyCount;
+        this.checkedTodoRate = checkedTodoRate;
+        this.startDt = startDt;
+        this.endDt = endDt;
     }
 
     public int increaseCopyCount(){
