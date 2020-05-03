798358973951812b586dcd2934cd0c79300bdf63
hscom96 <hscom96@gmail.com>
2020-05-04 05:16:38 +0900

    불필요한 패키지 코드 정리

--- src/main/java/com/eroom/erooja/features/todo/service/TodoService.java
+++ src/main/java/com/eroom/erooja/features/todo/service/TodoService.java
@@ -2,6 +2,7 @@ package com.eroom.erooja.features.todo.service;
 
 import com.eroom.erooja.common.exception.EroojaException;
 import com.eroom.erooja.domain.model.MemberGoal;
+import com.eroom.erooja.domain.model.Todo;
 import com.eroom.erooja.features.todo.dto.AddTodoDTO;
 import com.eroom.erooja.features.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,8 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final ModelMapper modelMapper;
 
-    public List<com.eroom.erooja.domain.model.Todo> addTodo(String uid, Long goalId, List<AddTodoDTO> todoDTODTOList) {
-        List<com.eroom.erooja.domain.model.Todo> todoList = mapDTOtoTodo(uid, goalId, todoDTODTOList);
+    public List<Todo> addTodo(String uid, Long goalId, List<AddTodoDTO> todoDTODTOList) {
+        List<Todo> todoList = mapDTOtoTodo(uid, goalId, todoDTODTOList);
 
         if (checkPriorityIsNotCorrect(todoList))
             throw new EroojaException(TODO_PRIORITY_NOT_CORRECT);
@@ -30,25 +31,27 @@ public class TodoService {
         return todoRepository.saveAll(todoList);
     }
 
-    public Boolean checkPriorityIsNotCorrect(List<com.eroom.erooja.domain.model.Todo> todoList) {
+    public Boolean checkPriorityIsNotCorrect(List<Todo> todoList) {
         int checkCount = 0;
-        for (com.eroom.erooja.domain.model.Todo todo : todoList) {
+        for (Todo todo : todoList) {
             if (todo.getPriority() != checkCount++)
                 return true;
         }
         return false;
     }
 
-    public Page<com.eroom.erooja.domain.model.Todo> getTodoListByGoalIdAndUid(Pageable pageable, Long goalId, String uid){
+    public Page<Todo> getTodoListByGoalIdAndUid(Pageable pageable, Long goalId, String uid){
         return todoRepository.getTodoListByGoalIdAndUid(pageable, goalId, uid);
     }
 
-    public List<com.eroom.erooja.domain.model.Todo> mapDTOtoTodo(String uid, Long goalId, List<AddTodoDTO> todoDTODTOList) {
+    public List<Todo> mapDTOtoTodo(String uid, Long goalId, List<AddTodoDTO> todoDTODTOList) {
         return todoDTODTOList.stream()
                 .map(todoDTO -> {
-                    com.eroom.erooja.domain.model.Todo todo = modelMapper.map(todoDTO, com.eroom.erooja.domain.model.Todo.class);
+                    Todo todo = modelMapper.map(todoDTO, Todo.class);
                     todo.setMemberGoal(MemberGoal.builder().uid(uid).goalId(goalId).build());
                     return todo;
                 }).collect(Collectors.toList());
     }
+
+    public List<>
 }
