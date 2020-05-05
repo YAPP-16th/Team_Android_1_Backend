2f9be3ed4298ac6be524e072e0899ec8d5f5605c
hscom96 <hscom96@gmail.com>
2020-05-06 06:16:42 +0900

    목표수정 nullpointerException 해결

--- src/main/java/com/eroom/erooja/features/goal/dto/UpdateGoalRequestDTO.java
+++ src/main/java/com/eroom/erooja/features/goal/dto/UpdateGoalRequestDTO.java
@@ -10,21 +10,20 @@ import org.hibernate.validator.constraints.Length;
 @Getter
 @Setter
 public class UpdateGoalRequestDTO {
-    @Length(min=5, message = "목표명을 최소 5자 이상 입력해주세요.")
     @Length(max=50, message = "목표명은 50자 이내만 입력")
     private String title;
     private String description;
 
     @JsonIgnore
     public Boolean isTitleChanged(){
-        if(this.title.isEmpty())
+        if(this.title == null || this.title.isEmpty())
             return false;
         return true;
     }
 
     @JsonIgnore
     public Boolean isDescriptionChanged(){
-        if(this.description.isEmpty())
+        if(this.description == null || this.description.isEmpty())
             return false;
         return true;
     }
