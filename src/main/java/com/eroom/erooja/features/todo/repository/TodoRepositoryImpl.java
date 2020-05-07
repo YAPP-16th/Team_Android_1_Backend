46f2005d98bcfd16027f12fb66ac1b48b365e20f
hscom96 <hscom96@gmail.com>
2020-05-08 05:50:54 +0900

    페이징처리 오류 수정(할일조회, 참여자조회)

--- src/main/java/com/eroom/erooja/features/todo/repository/TodoRepositoryImpl.java
+++ src/main/java/com/eroom/erooja/features/todo/repository/TodoRepositoryImpl.java
@@ -1,16 +1,22 @@
 package com.eroom.erooja.features.todo.repository;
 
+import com.amazonaws.services.dynamodbv2.model.QueryResult;
 import com.eroom.erooja.domain.model.MemberGoal;
 import com.eroom.erooja.domain.model.QMembers;
 import com.eroom.erooja.domain.model.QTodo;
 import com.eroom.erooja.domain.model.Todo;
 import com.eroom.erooja.features.membergoal.dto.GoalJoinTodoDto;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.hibernate.Hibernate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -29,12 +35,21 @@ public class TodoRepositoryImpl implements TodoRepositoryCustom {
     }
 
     public Page<Todo> getTodoListByGoalIdAndUid(Pageable pageable, Long goalId, String uid){
-        QueryResults<Todo> results = queryFactory
+        JPAQuery<Todo> query = queryFactory
                 .selectFrom(todo)
                 .where(todo.memberGoal.goalId.eq(goalId),
                         todo.memberGoal.uid.eq(uid))
-                .fetchResults();
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
 
+        for (Sort.Order o : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(todo.getType(),
+                    todo.getMetadata());
+            query.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC,
+                    pathBuilder.get(o.getProperty())));
+        }
+
+        QueryResults<Todo> results = query.fetchResults();
         return new PageImpl<>(results.getResults(), pageable, results.getTotal());
     }
 }
