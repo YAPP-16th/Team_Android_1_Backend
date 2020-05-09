c354fd799a41818f65976c47362b703ca0592b0d
hscom96 <hscom96@gmail.com>
2020-05-10 05:26:35 +0900

    목표 할일 업데이트 validation추가

--- src/main/java/com/eroom/erooja/features/todo/dto/UpdateTodoRequestDTO.java
+++ src/main/java/com/eroom/erooja/features/todo/dto/UpdateTodoRequestDTO.java
@@ -2,6 +2,7 @@ package com.eroom.erooja.features.todo.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Builder
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 public class UpdateTodoRequestDTO {
+    @NotNull(message="goalId를 전송해야합니다.")
     public Long goalId;
     public List<UpdateTodoDTO> todoList;
 }
