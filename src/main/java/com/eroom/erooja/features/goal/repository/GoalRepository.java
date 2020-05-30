6020a68879efe297cf383cf79a9e7549272a8451
hscom96 <hscom96@gmail.com>
2020-05-30 16:58:37 +0900

    목표 탐색 필터링(종료된 목표)

--- src/main/java/com/eroom/erooja/features/goal/repository/GoalRepository.java
+++ src/main/java/com/eroom/erooja/features/goal/repository/GoalRepository.java
@@ -16,6 +16,7 @@ public interface GoalRepository extends JpaRepository<Goal, Long>, JpaSpecificat
     @Query("SELECT g FROM Goal g JOIN GoalJobInterest i " +
             "ON g.id = i.goal.id " +
             "AND i.jobInterest.id = :interestId " +
+            "AND g.isEnd = false " +
             "AND g.id not in (SELECT m.goalId FROM MemberGoal m WHERE m.uid = :uid) ")
     Page<Goal> findGoalByInterestId(Long interestId, String uid, Pageable pageable);
 
